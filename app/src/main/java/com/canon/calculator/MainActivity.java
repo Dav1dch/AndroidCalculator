232b3710ca68af5b9a8c78096bd689ea23777df7
Dav1dch <807557852@qq.com>
2019-09-23 16:04:21 +0800

    add bottun and color

--- app/src/main/java/com/canon/calculator/MainActivity.java
+++ app/src/main/java/com/canon/calculator/MainActivity.java
@@ -3,8 +3,30 @@ package com.canon.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonAC;
+    Button buttonDel;
+    Button buttonPercent;
+    Button buttonDivide;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button buttonMuti;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button buttonMinus;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button buttonPlus;
+    Button buttonDot;
+    Button button0;
+    Button buttonEqual;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
