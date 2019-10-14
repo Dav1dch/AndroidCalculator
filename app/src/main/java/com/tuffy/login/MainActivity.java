c55c855c5f7ac312dcf191dd23ff81f5f6765026
Dav1dch <807557852@qq.com>
2019-10-14 15:04:17 +0800

    finish

--- app/src/main/java/com/tuffy/login/MainActivity.java
+++ app/src/main/java/com/tuffy/login/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,11 +16,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView count;
     EditText userName;
     EditText password;
+    private Integer times = 3;
     private void init(){
         count = findViewById(R.id.counts);
+        count.setText(" " + times.toString());
         userName = findViewById(R.id.userName);
         password = findViewById(R.id.password);
         findViewById(R.id.login).setOnClickListener(this);
+        findViewById(R.id.exit).setOnClickListener(this);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +34,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        userName.setText(password.getText());
-        String passwd = "123456";
-        if (!password.getText().toString().equals(passwd)){
-            Toast.makeText(this,"密码错误", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this,"登陆成功", Toast.LENGTH_SHORT).show();
+        switch (v.getId()){
+            case R.id.login:
+                String passwd = "123456";
+                if (!password.getText().toString().equals(passwd)){
+                    Toast.makeText(this,"密码错误", Toast.LENGTH_SHORT).show();
+                    times--;
+                    count.setText(" "+times.toString());
+                    if (times == 0){
+                        findViewById(R.id.login).setClickable(false);
+                        Toast.makeText(this,"无法登录", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    times = 3;
+                    Toast.makeText(this,"登陆成功", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.exit:
+                System.exit(0);
+                break;
         }
 
     }
